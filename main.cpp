864bbe8e60db9969a3cb5447a88dc0d293503430
McStecca <andreastecca2001@gmail.com>
2021-12-01 22:19:32 +0100

    Added hello world and comments

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "mainwindow.h"
+#include <iostream> //!da togliere
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+    std::cout<<"ciao mondo!"; //!da togliere
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
