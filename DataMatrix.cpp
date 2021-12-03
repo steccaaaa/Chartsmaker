04644f4c9b35a7ef9d38ee631e07b984809172bc
McStecca <andreastecca2001@gmail.com>
2021-12-03 17:00:37 +0100

    Co-authored-by: giuliadentone <giuliadentone@users.noreply.github.com>

--- DataMatrix.cpp
+++ DataMatrix.cpp
@@ -1,1 +1,54 @@
 #include "DataMatrix.h"
+
+void DataMatrix::deleteRow(unsigned int k)
+{
+    double *newData = new double[(rowNumber - 1) * columnNumber]; //new empty array with one less row allocated in the heap
+
+    for (unsigned int i = 0; i < columnNumber; ++i) //to scan the matrix
+    {
+        for (unsigned int j = 0; j < rowNumber; ++j)
+        {
+            if (j != k) //if my row selector is not equal to the row the user wants to delete i have to copy data on the new array
+            {
+                newData[] = data[]; //fills the nwe array with old the data besides the ones in the index k row
+            }
+        }
+    }
+    delete[] data;  //delete the content of the old array
+    data = newData; //now the data pointer points to the new array created in the function without a row
+    rowNumber--;    //decrement rowNumber
+}
+
+// 1 2 3 4 5 6 7 8 9 10 11 12
+
+// 00 01 02 03
+// 10 11 12 13
+// 20 21 22 23
+
+void DataMatrix::deleteColumn(unsigned int h)
+{
+    double *newData = new double[rowNumber * (columnNumber - 1)]; //new empty array without a column in the heap
+
+    for (unsigned int i = 0; i < rowNumber; ++i) //scan the array/matrix
+    {
+        for (unsigned int j = 0; j < columnNumber; ++j)
+        {
+            //fills new array with the old data without the deleted column
+            if (j < h)
+                newData[i * j] = data[i * j];
+            else if (j > h)
+                newData[i * (j - 1)] = data[i * j];
+        }
+    }
+    delete[] data;  //deletes old array
+    data = newData; //replaces the data address with the newer one
+    columnNumber--; //decrement columnNumber
+}
+
+DataMatrix DataMatrix::addRow(double *a, unsigned int k)
+{
+}
+
+DataMatrix DataMatrix::addColumn(double *a, unsigned int h)
+{
+}
--- DataMatrix.h
+++ DataMatrix.h
@@ -4,24 +4,43 @@
 class DataMatrix
 {
 private:
-    double *data;
+    double *data;              //pointer to an two dimensions array
+    unsigned int rowNumber;    //number of rows
+    unsigned int columnNumber; //number of columns
 
 public:
-    DataMatrix(/* args */);
+    DataMatrix(double *data, unsigned int rowNumber, unsigned int columnNumber = 1);
     ~DataMatrix();
+
+    /**
+     * @brief adds a row in n poition
+     * 
+     * @param a pointer to the array to add
+     * @param n number of the column to replace
+     */
+    void addRow(double *a, unsigned int n = 0);
+
+    /**
+     * @brief adds a column in n position
+     * 
+     * @param a pointer to the array to add
+     * @param n number of the row to replace
+     */
+    void addColumn(double *a, unsigned int n = 0);
+
+    /**
+     * @brief deletes the row in k index
+     * 
+     * @param k index of row to remove
+     */
+    void deleteRow(unsigned int k);
+
+    /**
+     * @brief deletes the column in h index
+     * 
+     * @param h index of column to remove
+     */
+    void deleteColumn(unsigned int h);
 };
-
-DataMatrix::DataMatrix(/* args */)
-{
-}
-
-DataMatrix::~DataMatrix()
-{
-}
-
-void addRow(&);
-void addColumn(&);
-void deleteRow(unsigned int);
-void deleteColumn(unsigned int);
 
 #endif
