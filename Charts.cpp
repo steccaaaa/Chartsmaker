d3e4016fde0e054497c68abe5e8bfe1efab1c8c9
giuliadentone <85439911+giuliadentone@users.noreply.github.com>
2021-12-15 21:34:58 +0100

    Update Charts.cpp
    abbozzato roundchart

--- Charts.cpp
+++ Charts.cpp
@@ -17,10 +17,59 @@ DataMatrix Chart::getData()
 
 QChart RoundChart::draw(QChart chart)
 {
-    //getdata per la prima colonna
-    //get data per le labels
-    //qpieseries e attacco dati a nomi
-    //roba grafica
+    series = new QPieseries();
+    series = append( //lable; data from matrix con getters);
+    series = append( //lable; data from matrix con getters);
+    series = append( //lable; data from matrix con getters);
+    QPieSlice *slice = new QPieSlice();
+    slice->setLabel("label")
+    slice->setValue(//...)
+    series->append(std::move(slice));
+    for(int i = 0; i < series->count(); ++i){
+        series->slices.at(i)->setLabelVisible(true);
+        if(i == 0)
+        {
+            seres->slices().at(i)->setColor(Qt::black);
+            series->slices().at(i)->setLabelPosition(QPieSLice::LabelInsideNormal);
+            QFont m_font = series->slices().at(i)->labelfont();
+            m_font.setPointSize(12);
+            m_font.setBold(true);
+            m_font.setItalic(true);
+            series->slices().at(i)->setLabelFont(m_font);
+                         
+        }
+        else if(i == 1)
+        {
+           seres->slices().at(i)->setColor(Qt::black);
+           series->slices().at(i)->setLabelPosition(QPieSlice::LabelInsideOrizontal);
+            QFont m_font = series->slices().at(i)->labelfont();
+            m_font.setPointSize(12);
+            m_font.setBold(true);
+            m_font.setItalic(true);
+            series->slices().at(i)->setLabelFont(m_font);
+                         
+        }
+        else if(i == 2){
+            seres->slices().at(i)->setColor(Qt::black);
+            series->slices().at(i)->setLabelPosition(QPieSlice::LabelInsideTangential);
+            QFont m_font = series->slices().at(i)->labelfont();
+            m_font.setPointSize(12);
+            m_font.setBold(true);
+            m_font.setItalic(true);
+            series->slices().at(i)->setLabelFont(m_font);
+                         
+        }
+        else(i == 3)
+        {
+            seres->slices().at(i)->setColor(Qt::black);
+            series->slices().at(i)->setLabelPosition(QPieSlice::LabelOutside);
+            QFont m_font = series->slices().at(i)->labelfont();
+            m_font.setPointSize(12);
+            m_font.setBold(true);
+            m_font.setItalic(true);
+            series->slices().at(i)->setLabelFont(m_font);      
+        }
+    }       
 }
 
 QChart PieChart::draw(QChart chart)
@@ -42,3 +91,5 @@ QChart DonutChart::draw(QChart chart)
 QChart LineChart::draw(QChart chart)
 {
 }
+        
+
