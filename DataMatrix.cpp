7dbd09fb210f8a4b1ba862ca2b161fb551630143
McStecca <andreastecca2001@gmail.com>
2021-12-19 15:17:55 +0100

    added print, row methods now work

--- DataMatrix.cpp
+++ DataMatrix.cpp
@@ -1,10 +1,13 @@
 #include "DataMatrix.h"
 #include <vector>
-#include <iostream> //! da togliere
+#include <iostream>
+using std::vector;
 
 void print(std::vector<std::vector<double>> &vec) //! debug
 {
-    std::cout << vec.size() << "\n";
+
+    ////std::cout << "printing data &\n";
+    ////std::cout << vec.size() << "\n";
     for (int i = 0; i < vec.size(); i++)
     {
         for (int j = 0; j < vec[i].size(); j++)
@@ -17,40 +20,42 @@ void print(std::vector<std::vector<double>> &vec) //! debug
 
 void print(std::vector<std::vector<double>> *vec) //! debug
 {
+    std::cout << "printing data *\n";
     print(*vec); // usa l'altro print e magicamente va tutto
-    /*
-        for (int i = 0; i < vec->size(); i++)
-        {
-            for (int j = 0; j < vec[i].size(); j++)
-            {
-                std::cout << (*vec)[i][j] << " ";
-            }
-            std::cout << std::endl;
-        }*/
 }
 
-using std::vector;
+void print(vector<std::string> &label)
+{
+    for (int i = 0; i < label.size(); i++)
+    {
+        std::cout << label[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void print(vector<std::string> *label)
+{
+    print(*label);
+}
 
 DataMatrix::DataMatrix(vector<vector<double>> &_data, vector<std::string> &_rowLabel, vector<std::string> &_columnLabel) // Costructor for DataMatrix
 {
-    // vector<vector<double>> *data = new vector<vector<double>>; //our DataMatrix is a vector of vectors
-    /*for (unsigned int i = 0; i < _data->size(); i++)           //to insert values in the vector
-        data[i] = _data[i];*/
-    // data = _data;
     data = new auto(_data);
-    // data->shrink_to_fit();
-    std::cout << "printing data & \n";
-    // print(data);
-    auto rowLabel = _rowLabel;       //! inculato
-    auto columnLabel = _columnLabel; //! inculato
+    print(data);
+    rowLabel = new auto(_rowLabel);
+    std::cout << "printing rowLabel \n";
+    print(rowLabel);
+    columnLabel = new auto(_columnLabel);
+    std::cout << "printing columnLabel \n";
+    print(columnLabel);
 }
 
 DataMatrix::DataMatrix(DataMatrix &_table) : data(_table.getData()), rowLabel(_table.getRowLabel()), columnLabel(_table.getColumnLabel()) {}
 
 void DataMatrix::addRow(vector<double> &v, unsigned int position) // adds a row in the chosen poition
 {
-    std::cout << "printing data *";
     print(data); //! debug
+    std::cout << "adding a row \n";
     data->insert(data->begin() + position, v);
     print(data); //! debug
 }
@@ -59,14 +64,16 @@ void DataMatrix::deleteRow(unsigned int position) // deletes the row in the chos
 {
     if (position < data->size())
         data->erase(data->begin() + position);
+    std::cout << "deleting row";
+    print(data);
 }
 
 void DataMatrix::addColumn(vector<double> &v, unsigned int position) // adds a column in the chosen position
 {
     for (int i = 0; i < data->size(); ++i)
     {
-        // data->insert(data[i].begin() + position, v[i]);
-        // data[i].insert(position, v[i]);
+        //data->insert(data[i].begin() + position, v[i]);
+        //data[i].insert(position, v[i]);
     }
 }
 
