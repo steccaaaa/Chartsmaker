7a10d9e9ba5bd169c38ede737f5da13672c7f15c
McStecca <andreastecca2001@gmail.com>
2021-12-21 14:32:06 +0100

    addrow e column devono aggiungere anche una label

--- DataMatrix.h
+++ DataMatrix.h
@@ -6,16 +6,16 @@
 class DataMatrix
 {
 private:
-    std::vector<std::vector<double>> *data; //reference of a 2D vector
-    std::vector<std::string> *rowLabel;     //vector of labels for the rows
-    std::vector<std::string> *columnLabel;  //vector of labels for the columns
+    std::vector<std::vector<double>> *data; // reference of a 2D vector
+    std::vector<std::string> *rowLabel;     // vector of labels for the rows
+    std::vector<std::string> *columnLabel;  // vector of labels for the columns
 
 public:
     DataMatrix() : data(nullptr), rowLabel(nullptr), columnLabel(nullptr) {}
 
     /**
      * @brief Construct a new Data Matrix object
-     * 
+     *
      * @param data reference of a 2d vector
      * @param rowLabel reference of a string vector
      * @param columnLabel reference of a string vector
@@ -24,64 +24,66 @@ public:
 
     /**
      * @brief Copy constructor of a new Data Matrix object
-     * 
-     * @param table 
+     *
+     * @param table
      */
     DataMatrix(DataMatrix &table);
     /**
      * @brief Destructor of Data Matrix object
-     * 
+     *
      */
     ~DataMatrix();
 
     /**
      * @brief adds a row in n poition
-     * 
+     *
      * @param a reference to the vector to add
      * @param n index in which to insert the row
+     * @param label label of the new row
      */
-    void addRow(std::vector<double> &a, unsigned int n = 0);
+    void addRow(std::vector<double> &a, unsigned int n = 0, std::string label = "");
 
     /**
      * @brief adds a column in n position
-     * 
+     *
      * @param a reference to the vector to add
      * @param n index in which to insert the column
+     * @param label label of the new column
      */
-    void addColumn(std::vector<double> &a, unsigned int n = 0);
+    void addColumn(std::vector<double> &a, unsigned int n = 0, std::string label = "");
 
     /**
      * @brief deletes the row in n index
-     * 
+     *
      * @param n index of the row to remove
      */
     void deleteRow(unsigned int n);
 
     /**
      * @brief deletes the column in n index
-     * 
+     *
      * @param n index of the column to remove
      */
     void deleteColumn(unsigned int n);
 
     /**
      * @brief Get the Data object
-     * 
-     * @return std::vector<std::vector<double>> 
+     *
+     * @return std::vector<std::vector<double>>
      */
     std::vector<std::vector<double>> *getData();
 
     /**
      * @brief Get the Row Label object
-     * 
-     * @return std::vector<string>* 
+     *
+     * @return std::vector<string>*
      */
     std::vector<std::string> *getRowLabel();
 
     /**
      * @brief Get the Column Label object
-     * 
-     * @return std::vector<std::string>* 
+     *
+     * @return std::vector<std::string>*
      */
     std::vector<std::string> *getColumnLabel();
 };
