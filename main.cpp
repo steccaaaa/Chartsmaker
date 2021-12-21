18ac965107f16360d6a01b0a1b11d0d8bdea4cd6
McStecca <andreastecca2001@gmail.com>
2021-12-21 22:52:59 +0100

    tolti gli arg dal main

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ void print(std::vector<std::vector<double>> vec) //! debug
     }
 }
 
-int main(int argc, char *argv[])
+int main()
 {
     std::vector<std::vector<double>> mat{
         {1, 2, 3},
@@ -25,6 +25,11 @@ int main(int argc, char *argv[])
     //DataMatrix x = DataMatrix(mat, mats, mats);
     DataMatrix x(mat, mats, mats);
     std::vector<double> mario{1, 3, 7};
-    x.addRow(mario, 2);
-    //mat[0][0] = 2;
+    std::vector<double> mario4{6, 7, 4, 4};
+
+    x.addRow(mario, 3, "yoda");
+    x.addColumn(mario4, 2, "luke");
+    mat[0][0] = 200;
+    x.deleteRow(2);
+    x.deleteColumn(0);
 }
