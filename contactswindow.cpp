98a840f07adf462bce337576afa985dc5ce194d4
McStecca <andreastecca2001@gmail.com>
2022-01-03 15:27:31 +0100

    mail cliccabile
    Co-authored-by: giuliadentone <giuliadentone@users.noreply.github.com>

--- contactswindow.cpp
+++ contactswindow.cpp
@@ -6,14 +6,22 @@ contactswindow::contactswindow(QWidget *parent) : QWidget(parent)
 {
     QVBoxLayout *layout = new QVBoxLayout(this);
 
-    QLabel *label = new QLabel("Developers", this);
+    QLabel *label = new QLabel("Developers:", this);
     QLabel *label2 = new QLabel("Dentone Giulia", this);
-    QLabel *label3 = new QLabel("Email: giulia.dentone@studenti.unipd.it", this);
-    QLabel *label4 = new QLabel("Student in Computer Science at the University of Padua", this);
+    QLabel *label3 = new QLabel("<a href=\"mailto:giulia.dentone@studenti.unipd.it\">giulia.dentone@studenti.unipd.it</a>", this);
+    QLabel *label4 = new QLabel("Computer Science Student at the University of Padua", this);
 
     QLabel *label5 = new QLabel("Stecca Andrea", this);
-    QLabel *label6 = new QLabel("Email: andrea.stecca.1@studenti.unipd.it", this);
-    QLabel *label7 = new QLabel("Student in Computer Science at the University of Padua", this);
+    QLabel *label6 = new QLabel("<a href=\"mailto:andrea.stecca.1@studenti.unipd.it\">andrea.stecca.1@studenti.unipd.it</a>", this);
+    QLabel *label7 = new QLabel("Computer Science Student at the University of Padua", this);
+
+    label3->setTextFormat(Qt::RichText);
+    label3->setTextInteractionFlags(Qt::TextBrowserInteraction);
+    label3->setOpenExternalLinks(true);
+
+    label6->setTextFormat(Qt::RichText);
+    label6->setTextInteractionFlags(Qt::TextBrowserInteraction);
+    label6->setOpenExternalLinks(true);
 
     layout->addWidget(label);
     layout->addWidget(label2);
