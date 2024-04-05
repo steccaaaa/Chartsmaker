9eb8fb65eadc7b89790aed61dbac66806ebfc4fa
Docente <Docente@serverlab.local>
2024-04-05 10:06:34 +0200

    a

--- main.cpp
+++ main.cpp
@@ -1,4 +1,20 @@
+#include <QApplication>
 
+#include "model.h"
+#include "mainwindow.h"
+#include "controller.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    Controller c;
+    Model m;
+    MainWindow mw;
+
+    c.setModel(&m);
+    c.setMainWindow(&mw);
+    mw.setController(&c);
 
     c.init();
 
