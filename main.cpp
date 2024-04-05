6d26b0898f53cf52abf4195731e9b213c7e28723
Docente <Docente@serverlab.local>
2024-04-05 09:59:32 +0200

    cmmit test

--- main.cpp
+++ main.cpp
@@ -1,20 +1,4 @@
-#include <QApplication>
 
-#include "model.h"
-#include "mainwindow.h"
-#include "controller.h"
-
-int main(int argc, char *argv[])
-{
-    QApplication app(argc, argv);
-
-    Controller c;
-    Model m;
-    MainWindow mw;
-
-    c.setModel(&m);
-    c.setMainWindow(&mw);
-    mw.setController(&c);
 
     c.init();
 
